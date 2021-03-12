f0808129d9c2bed250eb7cdc66fff3478df78000
史祥宇 <sxy@dataqin.com>
2021-03-12 11:25:46 +0800

    '修改dockerfile'

--- src/main/java/com/example/workflow/camunda/ExternalTask.java
+++ src/main/java/com/example/workflow/camunda/ExternalTask.java
@@ -13,44 +13,44 @@ import java.util.List;
 @Component
 public class ExternalTask {
 
-    static{
-        System.out.println("execute");
-        ExternalTaskClient client = ExternalTaskClient.create()
-                .baseUrl("http://localhost:8000/engine-rest")
-                .workerId("some-random-id")
-                .build();
-        client.subscribe("creditScoreChecker")
-                .lockDuration(1000)
-                .handler((externalTask, externalTaskService) -> {
-
-                    // retrieve a variable from the Workflow Engine
-                    // int defaultScore = externalTask.getVariable("defaultScore");
-
-                    List<Integer> creditScores = new ArrayList<>(Arrays.asList(5, 9, 1, 4, 10));
-
-                    // create an object typed variable
-                    ObjectValue creditScoresObject = Variables
-                            .objectValue(creditScores)
-                            .create();
-
-                    // complete the external task
-                    externalTaskService.complete(externalTask,
-                            Collections.singletonMap("creditScores", creditScoresObject));
-
-                    System.out.println("The External Task " + externalTask.getId() + " has been completed!");
-
-                }).open();
-        // subscribe to the topic
-        client.subscribe("loanGranter").lockDuration(1000).handler((externalTask, externalTaskService)->{
-            System.out.println("The External Task " + externalTask.getId() + " has been completed!");
-            externalTaskService.complete(externalTask);
-        }).open();
-        client.subscribe("send-message").lockDuration(1000).handler((externalTask, externalTaskService)->{
-            System.out.println("The External Task " + externalTask.getId() + " has been completed!");
-            externalTaskService.complete(externalTask);
-        }).open();
-
-
-        }
+//    static{
+//        System.out.println("execute");
+//        ExternalTaskClient client = ExternalTaskClient.create()
+//                .baseUrl("http://localhost:8000/engine-rest")
+//                .workerId("some-random-id")
+//                .build();
+//        client.subscribe("creditScoreChecker")
+//                .lockDuration(1000)
+//                .handler((externalTask, externalTaskService) -> {
+//
+//                    // retrieve a variable from the Workflow Engine
+//                    // int defaultScore = externalTask.getVariable("defaultScore");
+//
+//                    List<Integer> creditScores = new ArrayList<>(Arrays.asList(5, 9, 1, 4, 10));
+//
+//                    // create an object typed variable
+//                    ObjectValue creditScoresObject = Variables
+//                            .objectValue(creditScores)
+//                            .create();
+//
+//                    // complete the external task
+//                    externalTaskService.complete(externalTask,
+//                            Collections.singletonMap("creditScores", creditScoresObject));
+//
+//                    System.out.println("The External Task " + externalTask.getId() + " has been completed!");
+//
+//                }).open();
+//        // subscribe to the topic
+//        client.subscribe("loanGranter").lockDuration(1000).handler((externalTask, externalTaskService)->{
+//            System.out.println("The External Task " + externalTask.getId() + " has been completed!");
+//            externalTaskService.complete(externalTask);
+//        }).open();
+//        client.subscribe("send-message").lockDuration(1000).handler((externalTask, externalTaskService)->{
+//            System.out.println("The External Task " + externalTask.getId() + " has been completed!");
+//            externalTaskService.complete(externalTask);
+//        }).open();
+//
+//
+//        }
 
 }
